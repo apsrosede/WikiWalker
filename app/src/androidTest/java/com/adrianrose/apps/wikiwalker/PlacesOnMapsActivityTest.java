754f23cfe9fd47064a4306d31c89bbcc736cbd4d
Adrian Rose <apsrosede@hotmail.com>
2015-08-03 15:39:59 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/adrianrose/apps/wikiwalker/PlacesOnMapsActivityTest.java
@@ -0,0 +1,13 @@
+package com.adrianrose.apps.wikiwalker;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by adrianrose on 03.08.15.
+ */
+public class PlacesOnMapsActivityTest extends TestCase {
+
+
+
+
+}
